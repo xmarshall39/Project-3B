9b550818cbc4b46c0ee3a71efd88da5170d1c691
xmarshall39 <48485277+xmarshall39@users.noreply.github.com>
2019-04-13 14:43:25 -0400

    Trial

--- Project 3/Assets/Scripts/Enemy/NearestCorner.cs
+++ Project 3/Assets/Scripts/Enemy/NearestCorner.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class NearestCorner : MonoBehaviour
 {
+
+    //Creates new chasers at random points off screen
+    //at regular inervals
+
+    //Create new turrets at random intervals and
+    //transform them to a corner of the screen
+
     // Start is called before the first frame update
     void Start()
     {
