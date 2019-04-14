50afaba41104b939f7ff19b735c7fb0362545298
xmarshall39 <48485277+xmarshall39@users.noreply.github.com>
2019-04-14 09:37:35 -0400

    WeaponFixes

---
+++ Project 3/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    public float time;
+    private Scene currentScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentScene == SceneManager.GetSceneByName("GameScene"))
+            time += Time.deltaTime;
+    }
+
+
+    //Restarts the timer when the gameScene is reloaded
+    private void OnEnable()
+    {
+        PlayGame.OnGameStart += RestartTimer;
+    }
+
+    private void OnDisable()
+    {
+        PlayGame.OnGameStart -= RestartTimer;
+    }
+
+    public void RestartTimer()
+    {
+        time = 0;
+    }
+}
